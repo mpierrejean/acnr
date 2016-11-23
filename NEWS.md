f4381c2d46c89993c1f946f9163d1ffcb009a22b
Pierre Neuvial <pneuvial@gmail.com>
2016-11-23 15:55:11 +0100

    * Moved to roxygen2 documentation using package 'Rd2roxygen' * Cleaned up DESCRIPTOIN * Passes R CMD check locally with one NOTE (long file paths)

--- NEWS.md
+++ NEWS.md
@@ -1,48 +1,68 @@
-Package: acnr
-=================
-Version: 0.2.5 [2016-05-04]
-o Add a new dataSet GSE13372 
-o Change directory structure in inst/exdata: now inst/exdata/dataSet/chipType
-o Save all files in '.rds'
-o Add a 0% dillution in 'GSE11976' dataset 
+# Package: acnr
 
-Version: 0.2.4 [2014-11-17]
-o Used tools::resaveRdaFiles to compress data
+## Version: 0.2.6 [2016-11-23]
 
-Version: 0.2.3 [2014-10-27]
-o Changed ‘Affymetrix’ data, package is now less than 5MB.
+* Moved to roxygen2 documentation using package 'Rd2roxygen'
+* Cleaned up DESCRIPTOIN
+* Passes R CMD check locally with one NOTE (long file paths)
 
-Version: 0.2.2 [2014-09-08]
-o Added a mini-vignette.
+## Version: 0.2.5 [2016-05-04]
 
-Version: 0.2.1 [2014-05-30]
-o Moved inst/testScripts/system/preprocessing to inst/preprocessing.
+* Add a new dataSet GSE13372 
+* Change directory structure in inst/exdata: now inst/exdata/dataSet/chipType
+* Save all files in '.rds'
+* Add a 0% dillution in 'GSE11976' dataset 
 
-Version: 0.2.0 [2014-05-30]
-o Changed ‘platform’ to ‘dataSet’.
+## Version: 0.2.4 [2014-11-17]
 
-Version: 0.1.7 [2014-05-27]
-o Changed ‘Illumina’ to ‘GSE11976’ and ‘Affymetrix’ to ‘GSE29172’. 
+* Used tools::resaveRdaFiles to compress data
 
-Version: 0.1.6 [2014-05-14]
-o Bumped up version number to trigger build of 'jointSeg' by R-forge.
+## Version: 0.2.3 [2014-10-27]
 
-Version: 0.1.5 [2014-02-20]
-o Added a succinct vignette.
+* Changed ‘Affymetrix’ data, package is now less than 5MB.
 
-Version: 0.1.4 [2013-12-04]
-o Changed (1,2) region in Illumina Data.
+## Version: 0.2.2 [2014-09-08]
 
-Version: 0.1.3 [2013-04-13]
-o Now 'c' is on the non-log scale for both Affy and Illumina.
-o In 'loadCnRegionData': more informative error message when desired
+* Added a mini-vignette.
+
+## Version: 0.2.1 [2014-05-30]
+
+* Moved inst/testScripts/system/preprocessing to inst/preprocessing.
+
+## Version: 0.2.0 [2014-05-30]
+
+* Changed ‘platform’ to ‘dataSet’.
+
+## Version: 0.1.7 [2014-05-27]
+
+* Changed ‘Illumina’ to ‘GSE11976’ and ‘Affymetrix’ to ‘GSE29172’. 
+
+## Version: 0.1.6 [2014-05-14]
+
+* Bumped up version number to trigger build of 'jointSeg' by R-forge.
+
+## Version: 0.1.5 [2014-02-20]
+
+* Added a succinct vignette.
+
+## Version: 0.1.4 [2013-12-04]
+
+* Changed (1,2) region in Illumina Data.
+
+## Version: 0.1.3 [2013-04-13]
+
+* Now 'c' is on the non-log scale for both Affy and Illumina.
+* In 'loadCnRegionData': more informative error message when desired
 tumor fraction is not available.
 
-Version: 0.1.2 [2013-03-15]
-o Added more regions for Illumina platform
+## Version: 0.1.2 [2013-03-15]
 
-Version: 0.1.1 [2013-02-07]
-o Renamed 'inst/extData' to 'inst/extdata'
+* Added more regions for Illumina platform
 
-Version: 0.1.0 [2013-01-31]
-o Created from package 'jointSeg'.
+## Version: 0.1.1 [2013-02-07]
+
+* Renamed 'inst/extData' to 'inst/extdata'
+
+## Version: 0.1.0 [2013-01-31]
+
+* Created from package 'jointSeg'.
--- R/loadCnRegionData.R
+++ R/loadCnRegionData.R
@@ -1,43 +1,54 @@
-
-loadCnRegionData <- structure(function(
-### Load real, annotated copy number data
+#' loadCnRegionData
+#' 
+#' Load real, annotated copy number data
+#' 
+#' This function is a wrapper to load real genotyping array data taken from
+#' dilution series from the Affymetrix GenomeWideSNP_6 chip type (Rasmussen et
+#' al, 2011) or from the Illumina HumanCNV370v1 chip type (Staaf et al, 2008)
+#' 
+#' @param dataSet microarray dataSet from which the data was generated.
+#' @param tumorFraction proportion of tumor cells in the "tumor" sample. Should
+#' be in .3, .5, .7, 1 if \code{dataSet=="GSE29172"}, and in .14,.34,.50,.79,1
+#' when \code{dataSet=="GSE11976"}.
+#' @return a data.frame containing copy number data for different types of copy
+#' number regions.  Columns:\describe{ \item{c}{Total copy number}
+#' \item{b}{Allele B fraction (a.k.a. BAF)} \item{region}{a character value,
+#' annotation label for the region.  Preferably encoded as \code{"(C1,C2)"},
+#' where \code{C1} denotes the minor copy number and \code{C2} denotes the
+#' major copy number.  For example, \describe{ \item{(1,1)}{Normal}
+#' \item{(0,1)}{Hemizygous deletion} \item{(0,0)}{Homozygous deletion}
+#' \item{(1,2)}{Single copy gain} \item{(0,2)}{Copy-neutral LOH}
+#' \item{(2,2)}{Balanced two-copy gain} \item{(1,3)}{Unbalanced two-copy gain}
+#' \item{(0,3)}{Single-copy gain with LOH} }} \item{muN}{the (germline)
+#' genotype of SNPs. By definition, rows with missing genotypes are interpreted
+#' as non-polymorphic loci (a.k.a. copy number probes).}}
+#' @author Morgane Pierre-Jean and Pierre Neuvial
+#' @references Staaf, J., Lindgren, D., Vallon-Christersson, J., Isaksson, A.,
+#' Goransson, H., Juliusson, G., ... & Ringn\'er, M. (2008). Segmentation-based
+#' detection of allelic imbalance and loss-of-heterozygosity in cancer cells
+#' using whole genome SNP arrays. Genome Biol, 9(9), R136.
+#' 
+#' GEO data set: http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE11976
+#' 
+#' Rasmussen, M., Sundstr\"om, M., Kultima, H. G., Botling, J., Micke, P.,
+#' Birgisson, H., Glimelius, B. & Isaksson, A. (2011). Allele-specific copy
+#' number analysis of tumor samples with aneuploidy and tumor heterogeneity.
+#' Genome Biology, 12(10), R108.
+#' 
+#' GEO data sets: http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE29172
+#' http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE26302
+#' @examples
+#' 
+#' affyDat <- loadCnRegionData(dataSet="GSE29172", tumorFraction=1)
+#' str(affyDat)
+#' 
+#' illuDat <- loadCnRegionData(dataSet="GSE11976", tumorFraction=.79)
+#' str(illuDat)
+#' 
+#' @export loadCnRegionData
+loadCnRegionData <- function(
     dataSet=c("GSE29172", "GSE11976", "GSE13372"),
-### microarray dataSet from which the data was generated.
-    tumorFraction=1
-### proportion of tumor cells in the "tumor" sample.
-### Should be in {.3, .5, .7, 1} if \code{dataSet=="GSE29172"},
-### in {0,.14,.34,.50,.79,1} when \code{dataSet=="GSE11976"}.
-### and in {0,1} when \code{dataSet=="GSE13372"}.
-    ){
-    ##details<<This function is a wrapper to load real genotyping array
-    ##data taken from dilution series from the Affymetrix
-    ##GenomeWideSNP_6 chip type (Rasmussen et al, 2011) or from the
-    ##Illumina HumanCNV370v1 chip type (Staaf et al, 2008)
-    
-    ##references<<Staaf, J., Lindgren, D., Vallon-Christersson, J.,
-    ##Isaksson, A., Goransson, H., Juliusson, G., ... & Ringn\'er,
-    ##M. (2008). Segmentation-based detection of allelic imbalance and
-    ##loss-of-heterozygosity in cancer cells using whole genome SNP
-    ##arrays. Genome Biol, 9(9), R136. 
-    
-    ##references<<GEO data set:
-    ##http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE11976
-    
-    ##references<<Rasmussen, M., Sundstr\"{o}m, M., Kultima, H. G., Botling,
-    ##J., Micke, P., Birgisson, H., Glimelius, B. & Isaksson,
-    ##A. (2011). Allele-specific copy number analysis of tumor samples
-    ##with aneuploidy and tumor heterogeneity. Genome Biology, 12(10),
-    ##R108.
-
-    ##references<<Chiang DY, Getz G, Jaffe DB, O'Kelly MJ et al.
-    ##High-resolution mapping of copy-number alterations with massively
-    ##parallel sequencing. Nat Methods 2009 Jan;6(1):99-103
-    
-    ##references<<GEO data sets:
-    ##http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE29172
-    ##http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE26302
-    ##http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE13372
-
+    tumorFraction=1){
     dataSet <- match.arg(dataSet)
     tumorFractions <- switch(dataSet,
                              GSE13372=c(0,1),
@@ -60,37 +71,11 @@ loadCnRegionData <- structure(function(
     relPath <- file.path("extdata", dataSet, chipType, filename)
     pathname <- system.file(relPath, package="acnr")
     readRDS(pathname)
-### a data.frame containing copy number data for different types of
-### copy number regions.  Columns:\describe{
-### \item{c}{Total copy number}
-### \item{b}{Allele B fraction (a.k.a. BAF)}
-### \item{region}{a character value, annotation label for the
-### region.  Preferably encoded as \code{"(C1,C2)"}, where \code{C1}
-### denotes the minor copy number and \code{C2} denotes the major copy
-### number.  For example,  
-###  \describe{
-###  \item{(1,1)}{Normal}
-###  \item{(0,1)}{Hemizygous deletion}
-###  \item{(0,0)}{Homozygous deletion}
-###  \item{(1,2)}{Single copy gain}
-###  \item{(0,2)}{Copy-neutral LOH}
-###  \item{(2,2)}{Balanced two-copy gain}
-###  \item{(1,3)}{Unbalanced two-copy gain}
-###  \item{(0,3)}{Single-copy gain with LOH}
-###  }}
-### \item{muN}{the (germline) genotype of SNPs. By definition, rows with
-###  missing genotypes are interpreted as non-polymorphic loci (a.k.a. copy
-###  number probes).}}
-}, ex=function() {
-    affyDat <- loadCnRegionData(dataSet="GSE29172", tumorFraction=1)
-    str(affyDat)
-    
-    illuDat <- loadCnRegionData(dataSet="GSE11976", tumorFraction=.79)
-    str(illuDat)
-})
-
+}
 ############################################################################
 ## HISTORY:
+## 2016-11-23
+## o Moved to roxygen2 documentation.
 ## 2013-04-13
 ## o More informative error message when desired tumor fraction is not
 ## available.
