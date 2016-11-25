cb68f8b0a3c53afe398f9172bbab79ba8f08dcb1
Pierre Neuvial <pneuvial@gmail.com>
2016-11-25 16:29:57 +0100

    minor update: sort tumor fractions in output

--- R/listTumorFractions.R
+++ R/listTumorFractions.R
@@ -8,7 +8,7 @@
 #' @examples
 #' dataSets <- listDataSets()
 #' fracs <- listTumorFractions(dataSets[1])
-#' 
+#'
 listTumorFractions <- function(dataSet) {
     ds <- listDataSets()
     if( !(dataSet %in% ds)) {
@@ -16,11 +16,11 @@ listTumorFractions <- function(dataSet) {
     }
     path <- system.file("extdata", package="acnr")
     patt <- "(.*),([0-9]+),cnRegions.rds"
-    
+
     dpath <- file.path(path, dataSet)
     chipType <- list.files(dpath)
     dpath <- file.path(dpath, chipType)
     fl <- list.files(dpath, pattern=patt)
     pcts <- as.numeric(gsub(patt, "\\2", fl))/100
-    pcts
+    sort(pcts)
 }
